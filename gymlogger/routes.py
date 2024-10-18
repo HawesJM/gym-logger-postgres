8d5e6806c90bdc1c69c0f3a71ee2dfb3e2486f78
Joshua Hawes <joshua.m.hawes@gmail.com>
2024-10-18 07:41:17 +0000

    style form for 7 exercises

--- gymlogger/routes.py
+++ gymlogger/routes.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route("/categories")
 def categories():
-    categories = Category.query.order_by(Category.category_name).all()
+    categories = list(Category.query.order_by(Category.category_name).all())
     return render_template("categories.html", categories=categories)
 
 
@@ -24,7 +24,8 @@ def add_category():
 
 @app.route("/exercises")
 def exercises():
-    return render_template("exercises.html")
+    exercises = list(Exercise.query.order_by(Exercise.exercise_title).all())
+    return render_template("exercises.html", exercises=exercises)
 
 @app.route("/add_exercise", methods=["GET", "POST"])
 def add_exercise():
@@ -37,3 +38,37 @@ def add_exercise():
         db.session.commit()
         return redirect(url_for("exercises"))
     return render_template("add_exercise.html")
+
+@app.route("/workouts")
+def workouts():
+    workouts = list(Workout.query.order_by(Workout.workout_title).all())
+    return render_template("workouts.html", workouts=workouts)
+
+@app.route("/add_workout", methods=["GET", "POST"])
+def add_workout():
+    if request.method == "POST":
+        workout = Workout(
+            workout_title = request.form.get("workout_title"),
+            workout_date_time = request.form.get("workout_date_time"),
+            workout_location = request.form.get("workout_location"),
+            exercise_one_name = request.form.get("exercise_one_name"),
+            exercise_one_category = request.form.get("exercise_one_category"),
+            exercise_one_modifier_one = request.form.get("exercise_one_modifier_one"),
+            exercise_one_modifier_two = request.form.get("exercise_one_modifier_two"),
+            exercise_one_modifier_three = request.form.get("exercise_one_modifier_three"),
+            exercise_one_total_one = request.form.get("exercise_one_total_one"),
+            exercise_one_total_two = request.form.get("exercise_one_total_two"),
+            exercise_one_total_three = request.form.get("exercise_one_total_three"),
+            exercise_two_name = request.form.get("exercise_two_name"),
+            exercise_two_category = request.form.get("exercise_two_category"),
+            exercise_two_modifier_one = request.form.get("exercise_two_modifier_one"),
+            exercise_two_modifier_two = request.form.get("exercise_two_modifier_two"),
+            exercise_two_modifier_three = request.form.get("exercise_two_modifier_three"),
+            exercise_two_total_one = request.form.get("exercise_two_total_one"),
+            exercise_two_total_two = request.form.get("exercise_two_total_two"),
+            exercise_two_total_three = request.form.get("exercise_two_total_three")
+        )
+        db.session.add(workout)
+        db.session.commit()
+        return redirect(url_for("workouts"))
+    return render_template("add_workout.html")
