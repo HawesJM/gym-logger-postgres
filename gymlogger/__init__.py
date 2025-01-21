a728958d5ab0963dd03d533d225718c0dbd8e796
Joshua Hawes <joshua.m.hawes@gmail.com>
2025-01-21 21:15:50 +0000

    edit init file for heroku deployment

--- gymlogger/__init__.py
+++ gymlogger/__init__.py
@@ -9,9 +9,17 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 mongo = PyMongo(app)
 
