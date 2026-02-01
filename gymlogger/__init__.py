7eb51e3e9117d6bb3730fe06db9cb5f5ab8f5e38
HawesJM <joshua.m.hawes@gmail.com>
2026-02-01 22:22:18 +0000

    amend settings again for heroku deployment

--- gymlogger/__init__.py
+++ gymlogger/__init__.py
@@ -12,20 +12,13 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-# ---------- SQLAlchemy config ----------
 if os.environ.get("DEVELOPMENT") == "True":
-    uri = os.environ.get("DB_URL")
-    if not uri:
-        raise RuntimeError("DB_URL is not set but DEVELOPMENT=True")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if not uri:
-        raise RuntimeError("DATABASE_URL is not set on Heroku")
-
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 
 mongo = PyMongo(app)
@@ -34,6 +27,6 @@ db = SQLAlchemy(
     engine_options={ 'connect_args': { 'connect_timeout': 1000 }}
 )
 
-db.init_app(run)
+db.init_app(app)
 
 from gymlogger import routes # noqa
